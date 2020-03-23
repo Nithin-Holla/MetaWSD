72f6005059852bdbd735dedab8c728ea076c23b5
Nithin_Holla <nithin.holla7@gmail.com>
2020-03-23 15:43:50 +0100

    Back to BERT-base | Include CLS and SEP tokens

--- models/base_models.py
+++ models/base_models.py
@@ -66,7 +66,7 @@ class BERTSequenceModel(nn.Module):
         self.embed_dim = model_params['embed_dim']
         self.hidden_size = model_params['hidden_size']
         self.dropout_ratio = model_params.get('dropout_ratio', 0)
-        self.bert = BertModel.from_pretrained('bert-large-cased')
+        self.bert = BertModel.from_pretrained('bert-base-cased')
         self.linear = nn.Sequential(nn.Linear(self.embed_dim, self.hidden_size),
                                     nn.ReLU(),
                                     nn.Dropout(p=self.dropout_ratio))
@@ -74,7 +74,7 @@ class BERTSequenceModel(nn.Module):
         self.bert.pooler.dense.weight.requires_grad = False
         self.bert.pooler.dense.bias.requires_grad = False
 
-        tunable_layers = {str(l) for l in range(20, 24)}
+        tunable_layers = {str(l) for l in range(8, 12)}
         for name, param in self.bert.named_parameters():
             if not set.intersection(set(name.split('.')), tunable_layers):
                 param.requires_grad = False
@@ -82,5 +82,6 @@ class BERTSequenceModel(nn.Module):
     def forward(self, input, input_len):
         attention_mask = (input.detach() != 0).float()
         output, _ = self.bert(input, attention_mask=attention_mask)
+        output = output[:, 1:-1, :]  # Ignore the output of the CLS and SEP tokens
         output = self.linear(output)
         return output
