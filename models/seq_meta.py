701e6e3e2d85fb400eb11b766feec7af7dc1cb88
Nithin_Holla <nithin.holla7@gmail.com>
2020-05-12 18:49:15 +0200

    Global metrics for metaphor

--- models/seq_meta.py
+++ models/seq_meta.py
@@ -227,7 +227,8 @@ class SeqMetaModel(nn.Module):
                     param.grad /= len(query_accuracies)
 
         if testing:
-            return support_losses, query_accuracies, query_precisions, query_recalls, query_f1s
+            return support_losses, query_accuracies, query_precisions, query_recalls, query_f1s, \
+                   all_predictions.tolist(), all_labels.tolist()
         else:
             return query_losses, query_accuracies, query_precisions, query_recalls, query_f1s
 
