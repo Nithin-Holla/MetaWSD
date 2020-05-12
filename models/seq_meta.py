636cf3ff8e64db9d9f08ba43fb74e924607c708b
Nithin_Holla <nithin.holla7@gmail.com>
2020-05-12 16:18:30 +0200

    Macro F1 for metaphor

--- models/seq_meta.py
+++ models/seq_meta.py
@@ -196,12 +196,7 @@ class SeqMetaModel(nn.Module):
 
                 query_loss /= n_batch + 1
 
-            if episode.base_task != 'metaphor':
-                accuracy, precision, recall, f1_score = utils.calculate_metrics(all_predictions,
-                                                                                all_labels, binary=False)
-            else:
-                accuracy, precision, recall, f1_score = utils.calculate_metrics(all_predictions,
-                                                                                all_labels, binary=True)
+            accuracy, precision, recall, f1_score = utils.calculate_metrics(all_predictions, all_labels, binary=False)
 
             logger.info('Episode {}/{}, task {} [query set]: Loss = {:.5f}, accuracy = {:.5f}, precision = {:.5f}, '
                         'recall = {:.5f}, F1 score = {:.5f}'.format(episode_id + 1, n_episodes, episode.task_id,
