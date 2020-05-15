fe2913d088cbcfa35046d01842bf27f77cfc8517
Nithin_Holla <nithin.holla7@gmail.com>
2020-05-15 12:07:10 +0200

    Command line args for evaluation

--- eval_metaphor.py
+++ eval_metaphor.py
@@ -39,10 +39,24 @@ if __name__ == '__main__':
     # Parse arguments
     parser = ArgumentParser()
     parser.add_argument('--config', dest='config_file', type=str, help='Configuration file', required=True)
+    parser.add_argument('--n_support', type=int, help='Number of support examples', required=True)
+    parser.add_argument('--trained_learner', type=float, help='Name of the trained model', required=True)
+    parser.add_argument('--output_lr', type=float, help='Output learning rate', default=-1)
+    parser.add_argument('--learner_lr', type=float, help='Learner learning rate', default=-1)
+    parser.add_argument('--num_updates', type=float, help='Number of per-task update steps', default=-1)
     args = parser.parse_args()
 
     # Load configuration
     config = load_config(args.config_file)
+    config['learner_params']['num_outputs']['metaphor'] = 2
+    config['num_shots']['metaphor'] = args.n_support
+    config['trained_learner'] = args.trained_learner
+    if args.output_lr != -1:
+        config['output_lr'] = args.output_lr
+    if args.learner_lr != -1:
+        config['learner_lr'] = args.learner.lr
+    if args.num_updates != -1:
+        config['num_updates'] = args.num_updates
     logger.info('Using configuration: {}'.format(config))
 
     # Set seeds for reproducibility
@@ -58,22 +72,29 @@ if __name__ == '__main__':
     # Paths for metaphor dataset
     metaphor_base_path = os.path.join(config['base_path'], '../data/vuamc/')
     metaphor_train_path = os.path.join(metaphor_base_path, 'VUA_cls_train.csv')
+    metaphor_val_path = os.path.join(metaphor_base_path, 'VUA_cls_val.csv')
     metaphor_test_path = os.path.join(metaphor_base_path, 'VUA_cls_test.csv')
 
     # Load metaphor train and test dataset
     logger.info('Loading the dataset for metaphor')
     metaphor_train_dataset = MetaphorClassificationDataset(metaphor_train_path, config['learner_params']['num_outputs']['metaphor'])
+    metaphor_val_dataset = MetaphorClassificationDataset(metaphor_val_path, config['learner_params']['num_outputs']['metaphor'])
     metaphor_test_dataset = MetaphorClassificationDataset(metaphor_test_path, config['learner_params']['num_outputs']['metaphor'])
     logger.info('Finished loading the dataset for metaphor')
 
     # Generate episodes for metaphor
     logger.info('Generating episodes for metaphor')
-    metaphor_episodes = utils.generate_metaphor_episode(train_dataset=metaphor_train_dataset,
-                                                        test_dataset=metaphor_test_dataset,
-                                                        n_support_examples=config['num_shots']['metaphor'],
-                                                        task='metaphor')
-    test_episodes.extend(metaphor_episodes)
-    logger.info('Finished generating {} episodes for metaphor'.format(len(metaphor_episodes)))
+    metaphor_val_episode = utils.generate_metaphor_episode(train_dataset=metaphor_train_dataset,
+                                                           test_dataset=metaphor_val_dataset,
+                                                           n_support_examples=config['num_shots']['metaphor'],
+                                                           task='metaphor')
+    val_episodes.extend(metaphor_val_episode)
+    metaphor_test_episode = utils.generate_metaphor_episode(train_dataset=metaphor_train_dataset,
+                                                            test_dataset=metaphor_test_dataset,
+                                                            n_support_examples=config['num_shots']['metaphor'],
+                                                            task='metaphor')
+    test_episodes.extend(metaphor_test_episode)
+    logger.info('Finished generating {} episodes for metaphor'.format(len(metaphor_test_episode)))
 
     # Initialize meta learner
     if config['meta_learner'] == 'maml':
@@ -90,5 +111,6 @@ if __name__ == '__main__':
         raise NotImplementedError
 
     # Meta-testing
-    meta_learner.testing(test_episodes)
+    meta_learner.testing(val_episodes)
+    # meta_learner.testing(test_episodes)
     logger.info('Meta-testing completed')
